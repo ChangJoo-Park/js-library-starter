e617cc2380112a3905dafde75e29a11f58533b72
ChangJoo Park <pcjpcj2@gmail.com>
2018-04-25 11:33:20 +0900

    Initialize Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const sum = (a, b) => {
+  return a + b
+}
+
+module.exports = {
+  sum
+}
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+/* global __dirname, require, module */
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const path = require('path')
+const env = require('yargs').argv.env // use --env with webpack 2
+const pkg = require('./package.json')
+
+let libraryName = pkg.name
+
+let plugins = []
+let outputFile
+
+if (env === 'build') {
+  plugins.push(
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        output: {
+          comments: false
+        },
+        minify: {},
+        compress: {
+          booleans: true
+        }
+      }
+    })
+  )
+  outputFile = libraryName + '.min.js'
+} else {
+  outputFile = libraryName + '.js'
+}
+
+const config = {
+  entry: path.join(__dirname, '/src/index.js'),
+  devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, '/lib'),
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  },
+  plugins: plugins
+}
+
+module.exports = config
